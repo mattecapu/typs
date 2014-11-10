74d0cfa8f2cb46cec8c9c040c62250f8686eddfd
Matteo Capucci <mattecapu@live.it>
2014-11-10 08:47:18 +0100

    library update: new features, new internal organization, bug fixing

--- typs.js
+++ typs.js
@@ -1,153 +1,209 @@
 /*!
-	Type validation tool
-	TODO: refactor it with "type checker" functions
+	Type checking and validation tool
+	Made with care by Matteo Capucci <mattecapu@live.it>
+	Nerd it on GitHub: https://github.com/mattecapu/types.js
 */
 
-var type = function() {
-	return new (function(args) {
+// a type signature object
+function Typs (args) {
+	
+	if(args.length === 0) args = [undefined];
+	
+	var constraints = [];
+	var add = (function(constraint) {
+		constraints.push(constraint);
+		return this;
+	}).bind(this);
 
-		var constraints = [];
-		var add = function(constraint) {
-			constraints.push(constraint);
-			return this;
-		};
+	// checks if obj satisfies all the constraints of this type signature
+	this.checkOn = function(obj) {
+		//console.log('i\'ve got', constraints.length, 'problems');
+		return (constraints.length === 0) || !constraints.some((constraint, i) => {
+			//if(!constraint(obj)) console.log('constraint',i,'failed');
+			return !constraint(obj);
+		});
+	};
+	// checks if the objects passed to type() statisfy this type signature
+	this.check = function() {
+		return !args.some(((obj) => {
+			return !this.checkOn(obj);
+		}).bind(this));
+	};
 
-		this.checkOn = function(obj) {
-			return !constraints.some((constraint) => {
-				return !constraint(obj);
-			});
-		};
-		this.check = function() {
-			return [].some.call(args, ((obj) => {
-				return !this.checkOn(obj);
-			}).bind(this));
-		};
+	// check if obj statisfies one or more type signatures
+	this.matchAny = function(types) {
+		return add((obj) => {
+			return !types.some((t) => { return typs(obj).isnt(t) });
+		});
+	};
+	this.isAny = function(types) {
+		return add((obj) => {
+			return typs(obj).matchAny(types).check();
+		}).check();
+	};
 
-		// check if an object statisfies one or more type signatures
-		this.satisfiesAny = function(types) {
-			return !types.some((t) => { return !t.checkOn(obj) });
-		};
+	// check if obj is null, undefined or NaN
+	this.notNull = function() {
+		return add((obj) => {
+			return obj === false || obj === 0 || obj === '' || !!obj;
+		});
+	};
 
-		// check if an object is null, undefined or NaN
-		this.notNull = function() {
-			return add((obj) => {
-				return obj === false || obj === 0 || obj !== '' || !!obj;
-			});
-		};
+	// numbers
+	var sgn_regex = /^-/;
+	this.number = function () {
+		return add((obj) => {
+			return (typs(obj).string().check()
+					|| typeof obj === 'number' || obj instanceof Number)
+					&& !isNaN(parseFloat(obj))
+					&& isFinite(obj.toString().replace(sgn_regex, ''))
+		});
+	};
 
-		// numbers
-		var sgn_regex = /^-/;
-		this.number = function () {
-			return add((obj) => {
-				return (type(obj).string().check()
-						|| typeof obj === 'number' || obj instanceof Number)
-						&& !isNaN(parseFloat(obj))
-						&& isFinite(obj.toString().replace(sgn_regex, ''))
-			});
-		};
+	// checks if obj is an integer
+	this.integer = function() {
+		return add((obj) => {
+			return typs(obj).number().check() && parseInt(obj) === parseFloat(obj)
+		});
+	};
 
-		this.integer = function() {
-			return add((obj) => {
-				return type(obj).number().check() && parseInt(obj) === parseFloat(obj)
-			});
-		};
+	// checks if obj is positive or negative
+	this.positive = function() {
+		return add((obj) => {
+			return typs(obj).number().check() && parseFloat(obj) >= 0;
+		});
+	};
+	this.negative = function() {
+		return add((obj) => {
+			return !typs(obj).positive().check();
+		});
+	};
 
-		this.positive = function() {
-			return add((obj) => {
-				return type(obj).number().check() && parseFloat(obj) >= 0;
-			});
-		};
-		this.negative = function() {
-			return add((obj) => {
-				return !type(obj).positive().check();
-			});
-		};
+	// checks if obj is different from 0
+	this.notZero = function() {
+		return add((obj) => {
+			return typs(obj).number().check() && parseFloat(obj) !== 0;
+		});
+	};
 
-		this.notZero = function() {
-			return add((obj) => {
-				return type(obj).number().check() && parseFloat(obj) !== 0;
-			});
-		};
+	// strings
+	this.string = function() {
+		return add((obj) => {
+			return typeof obj === 'string' || obj instanceof String
+		});
+	};
 
-		//strings
-		this.string = function() {
-			return add((obj) => {
-				return typeof obj === 'string' || obj instanceof String
-			});
-		};
+	// checks if obj.length respects the given constraints (also good for arrays)
+	this.len = function({min, max, exact}) {
+		var param_type = typs().notNull().integer().positive();
+		return add((obj) => {
+			return (param_type.checkOn(min) ? obj.length >= min : true)
+					&& (param_type.checkOn(max) ? obj.length <= max : true)
+					&& (param_type.checkOn(exact) ? obj.length === exact : true)
+		});
+	};
+	// checks string not-emptiness
+	this.notEmpty = function() {
+		return add((obj) => {
+			return typs(obj).len({min: 1}).check();
+		});
+	};
 
-		// (also good for arrays)
-		this.len = function({min, max, exact}) {
-			var param_type = type().notNull().integer().positive();
-			return add((obj) => {
-				return (param_type.checkOn(min) ? obj.length >= min : true)
-						&& (param_type.checkOn(max) ? obj.length <= max : true)
-						&& (param_type.checkOn(exact) ? obj.length === exact : true)
-			});
-		};
-		this.notEmpty = function() {
-			return add((obj) => {
-				return type(obj).len({min: 1}).check();
-			});
-		};
+	// arrays
+	this.array = function() {
+		return add(Array.isArray);
+	};
 
-		// arrays
-		this.array = function() {
-			return add(Array.isArray);
-		};
+	// objects
+	this.object = function() {
+		return add((obj) => {
+			return typeof obj === 'object'
+		});
+	};
 
-		// objects
-		this.object = function() {
-			return add((obj) => {
-				return typeof obj === 'object'
-			});
-		};
-		
-		// returns true if accessing obj[key] doesn't raise an exception
-		this.keyable = function() {
-			return add((obj) => {
-				return type(obj).satisfiesAny([
-					type().object(), 
-					type().array(),
-					type().string()
-				]);
-			});
-		};
+	// returns true if accessing obj[<key>] doesn't raise an exception
+	this.keyable = function() {
+		return add((obj) => {
+			return typs(obj).satisfiesAny([
+				typs().object(),
+				typs().array(),
+				typs().string()
+			]);
+		});
+	};
 
-		this.hasKeys = function(keys) {
-			return add((obj) => {
-				if(!type(obj).keyable().check()) return false;
-				return !keys.some((key) => {
-					return !type(obj[key]).notNull().check()
-				});
+	// checks if obj has all keys defined
+	this.hasKeys = function(keys) {
+		return add((obj) => {
+			if(!typs(obj).keyable().check()) return false;
+			return !keys.some((key) => {
+				return !typs(obj[key]).notNull().check()
 			});
+		});
+	};
+	
+	// checks if obj is an instance of the given class
+	this.instanceOf = function(class_func) {
+		if(!typs(class_func).func().check()) throw new Error('typs().instanceOf() wants a function class as its first parameter');
+		return add((obj) => {
+			return obj instanceof class_func;
+		});
+	};
+
+	// checks if type matches obj
+	this.match = function(type) {
+		if(!typs(type).object().check() && !(type instanceof Typs)) {
+			throw new Error('typs().match() wants a type signature object as its first parameter');
 		};
-		
-		// check if type_object type signature matches obj
-		this.match = function(type_object) {
-			if(!type(type_object).object().check()) throw new Error('type().match() wants a type signature object as its first parameter');
+		if(type instanceof Typs) {
 			return add((obj) => {
-				return !Object.keys(type_object).some((key) => {
-					if(type(type_object[key]).object().check()) {
-						//nested objects
-						return type(obj[key]).match(type_object[key]).check();
-					} else if(!type(type_object[key].checkOn).func().check()) {
-						// same-value fields
-						return type(obj[key]).equals(type_object[key]).check();
-					} else {
-						// simple type checks
-						return !type_object[key].checkOn(obj[key]);
-					}
-				});
+				return type.checkOn(obj);
 			});
-		};
-		
-		this.equals = function(value) {
-			return add((obj) => {
-				return value === obj;
+		}
+		return add((obj) => {
+			return !Object.keys(type).some((key) => {
+				if(typs(type[key]).object().check()) {
+					// nested objects
+					return typs(obj[key]).is(type[key]).check();
+				} else if(!typs(type[key].checkOn).func().check()) {
+					// same-value fields
+					return typs(obj[key]).equals(type[key]).check();
+				} else {
+					// simple type checks
+					return !type[key].checkOn(obj[key]);
+				}
 			});
-		};
-	})(arguments);
+		});
+	};
+	this.is = function(type) {
+		return add((obj) => {
+			return typs(obj).match(type).check();
+		}).check();
+	};
+	this.isnt = function(type) {
+		return add((obj) => {
+			return !typs(obj).is(type);
+		}).check();
+	};
+
+	// checks if value equals to obj
+	this.equals = function(value) {
+		return add((obj) => {
+			return value === obj;
+		});
+	};
+
+	// functions
+	this.func = function() {
+		return add((obj) => {
+			return typeof obj === 'function';
+		});
+	};
+
 };
 
-module.exports = type;
+var typs = function(/* variadic arguments */) {
+	return new Typs([].slice.call(arguments));
+};
+
+module.exports = typs;
